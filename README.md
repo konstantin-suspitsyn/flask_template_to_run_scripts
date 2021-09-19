cbd119e8d144483577c76d9dd815c947aaa30ac5
konstantin_s <konstantin.suspitsyn@gmail.com>
2021-09-19 18:45:19 +0300

    Added login and session Added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flask template for quick start
+
+Template was made to easily add UI for users to run Python scripts without *.bat files and all this staff
+
+Html pages are based on bootstrap v5.1.1
+
+Create MySQL base
--- hq/routes.py
+++ hq/routes.py
@@ -1,5 +1,5 @@
 from hq import app, mysql
-from flask import render_template, request, flash, url_for, redirect
+from flask import render_template, request, flash, url_for, redirect, session
 from hq.forms import RegisterForm
 from passlib.hash import sha256_crypt
 
@@ -17,6 +17,7 @@ def about_page():
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
+    # TODO: добавить проверку если пользователь уже вошел, не показывать ничего
     sql_user_check = """
     SELECT * FROM users
     WHERE 
@@ -56,10 +57,14 @@ def register():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login_page():
+    # TODO: добавить проверку если пользователь уже вошел, не показывать ничего
+
+    error_message = "Логин не существует или пароль некорректный"
+
     if request.method == 'POST':
         # Получаем имя и пароль из формы
         username = request.form['username']
-        password_candidate = request.form['password']
+        password_candidate = request.form['password_candidate']
 
         # Пытаемся получить наличие user по username
         cursor = mysql.connection.cursor()
@@ -69,11 +74,40 @@ def login_page():
             # Если user есть, сравниваем пароли
             data = cursor.fetchone()
             password = data['password']
-            if sha256_crypt.verify(password, password_candidate):
-                app.logger.info('PASSWORD MATCHED')
+
+            cursor.close()
+
+            # Важен порядок внутри sha256_crypt.verify()
+            if sha256_crypt.verify(password_candidate, password):
+                # Юзер существует, пароль корректный
+
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash("{}, добро пожаловать!".format(username), 'success')
+                return redirect(url_for('index'))
+
             else:
-                app.logger.info('WRONG PASSWORD')
+                return render_template('login.html', error_message=error_message)
         else:
-            app.logger.info('NO USER')
+            return render_template('login.html', error_message=error_message)
 
     return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    if session.get('logged_in'):
+        # Если пользователь залогинился
+        session.clear()
+        flash('До новых встреч!', 'success')
+        return redirect(url_for('index'))
+    else:
+        # Если пользователь не залогинился и просто тыкается по ссылкам
+        flash('Чтобы выйти, необходимо войти. Как вы вообще попали на эту ссылку?', 'danger')
+        return redirect(url_for('index'))
+
+
+@app.route('/dashboard')
+def dashboard():
+    pass
