556cc3621b5ea3684c56cc7d3b9a5fa8a8895d23
konstantin_s <konstantin.suspitsyn@gmail.com>
2021-10-01 22:57:57 +0300

    Added update and delete

--- hq/data_change/data_change_routes.py
+++ hq/data_change/data_change_routes.py
@@ -29,6 +29,7 @@ def master_data_template():
         u.id = last_user_id """)
 
     all_data = cursor.fetchall()
+    cursor.close()
 
     if data > 0:
         # Если что-то выгрузилось
@@ -36,8 +37,6 @@ def master_data_template():
     else:
         msg = 'Записей нет'
         return render_template(r'db_changes/master_data_template.html', msg=msg)
-
-    cursor.close()
 
 
 @app.route('/db-change/master_data_template/add_data', methods=['GET', 'POST'])
@@ -64,3 +63,54 @@ def add_data_to_md():
         return redirect(url_for('master_data_template'))
 
     return render_template(r'db_changes/add_data.html', form=form)
+
+
+@app.route('/db-change/master_data_template/edit_data/<string:data_id>', methods=['GET', 'POST'])
+@is_logged_in
+def edit_data_to_md(data_id):
+    cursor = mysql.connection.cursor()
+
+    result = cursor.execute("""SELECT * FROM master_data_example
+    where id = %s""", [data_id])
+
+    data_one = cursor.fetchone()
+
+    form = BaseDataChangeForm(request.form)
+
+    form.some_data.data = data_one['some_data']
+    form.some_property.data = data_one['property']
+
+    if request.method == 'POST' and form.validate():
+        title_of_md = request.form['some_data']
+        property_of_md = request.form['some_property']
+
+        cursor = mysql.connection.cursor()
+        # Получаем id текущего юзера
+        cursor.execute("SELECT id FROM users WHERE username = %s", [session['username']])
+        data = cursor.fetchone()
+        user_id = data['id']
+        cursor.execute("""
+        UPDATE master_data_example
+        SET master_data_example.some_data = %s, master_data_example.property = %s, 
+        master_data_example.last_user_id=%s, update_datetime = %s
+        WHERE master_data_example.id = %s""", (title_of_md, property_of_md, user_id, dt.datetime.now(), data_id))
+        mysql.connection.commit()
+        cursor.close()
+
+        flash('Запись обновлена', 'success')
+
+        return redirect(url_for('master_data_template'))
+
+    return render_template(r'db_changes/add_data.html', form=form)
+
+
+@app.route('/db-change/master_data_template/delete_data/<string:data_id>', methods=['POST'])
+@is_logged_in
+def delete_data_from_md(data_id):
+    cursor = mysql.connection.cursor()
+    cursor.execute("DELETE FROM master_data_example where id = %s", [data_id])
+    mysql.connection.commit()
+    cursor.close()
+
+    flash('Запись удалена', 'success')
+    return redirect(url_for('master_data_template'))
